081df462ad33572ebfe263628c858ab16a26e961
Jois Andresky <jois.andresky77@gmail.com>
2017-09-14 12:38:24 +0700

    Perubahan REST API HD DOSEN

---
+++ server/hd-dosen/perkuliahan/cek-alat.php
@@ -0,0 +1,84 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+include('../../koneksi.php');
+include('../../koneksi_sim.php');
+
+$data = json_decode(file_get_contents("php://input"));
+global $hasilNya, $hsl;
+$idAlat = '';
+$mhs = '';
+$dm = '';
+$wa = '';
+if(isset($_POST['nid'])) {
+  $NID = mysqli_real_escape_string($conn, $_POST['nid']);
+
+  $cariAlat = "SELECT id FROM `tb_session` WHERE `key` = 'DosenMasuk' AND val = '$NID';";
+  $cekAlat = mysqli_query($conn, $cariAlat);
+  while($row2 = mysqli_fetch_object($cekAlat)){
+    $idAlat = $row2->id;
+  }
+
+  if(isset($idAlat)){
+    $mhs = executeQuery($conn, $idAlat, "mhsabsen")->val;
+    $kls = executeQuery($conn, $idAlat, "dosenmengajarkelas")->val;
+    $jdk = executeQuery($conn, $idAlat, "dosenmengajarjadwal")->val;
+    $mk = executeQuery($conn, $idAlat, "dosenmengajarmatakuliah")->val;
+    $mata_kuliah = getMatkul($sim_con, $mk);
+    $ms = explode(",", $mhs);
+    array_splice($ms, 0, 1);
+    $mahasiswaNya = array();
+    for ($i = 0;$i < count($ms);$i++) {
+        $dt = getDetailMahasiswa($sim_con, $ms[$i]);
+        array_push($mahasiswaNya, $dt);
+    }
+
+    $data = array(
+      'mahasiswa' => $mahasiswaNya,
+      'kelas' => $kls,
+      'jadwal' => $jdk,
+      'matkul' => $mata_kuliah,
+      'err' => mysqli_error($conn),
+      'erro' => mysqli_error($sim_con)
+    );
+    echo json_encode($data);
+  } else {
+    echo json_encode($data);
+  }
+}
+
+function executeQuery($conn, $alat, $key){
+  $sql = "SELECT val from `tb_session` WHERE id = '$alat' AND `key` = '$key'";
+  $hasil = mysqli_query($conn, $sql);
+  while($row = mysqli_fetch_object($hasil)){
+    $hasilNya = $row;
+  }
+
+  return $hasilNya;
+}
+
+function getMatkul($sim_con, $mk){
+  $matkul;
+  $sql = "SELECT NMTBMK from tbmk WHERE KDTBMK = '$mk'";
+  $hasil = mysqli_query($sim_con, $sql);
+  while($row = mysqli_fetch_object($hasil)){
+    $matkul = $row->NMTBMK;
+  }
+  return $matkul;
+}
+
+function getDetailMahasiswa($sim_con, $npm){
+  $hsl = array();
+  $sql = "SELECT TIME(trabsenmhs.TGLTRABSEN) AS TGLTRABSEN, msmhs.NMMHSCMSMHS, msmhs.NIMHSCMSMHS FROM trabsenmhs LEFT JOIN msmhs ON trabsenmhs.NPMTRABSEN = msmhs.NIMHSCMSMHS WHERE trabsenmhs.NPMTRABSEN = '$npm' AND DATE(trabsenmhs.TGLTRABSEN) = '2017-08-26'";
+  $hasil = mysqli_query($sim_con, $sql);
+  while($row = mysqli_fetch_object($hasil)){
+    $hsl = $row;
+  }
+
+  if(count($hsl) > 0){
+    return $hsl;
+  } else {
+    return mysqli_error($sim_con);
+  }
+}
+
+?>
